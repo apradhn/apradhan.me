1193f159d5475c27730c6cd11903b97163155326
Abhijit Pradhan <abhijitpradhan@Vision.local>
2015-02-20 14:51:42 -0600

    Added profile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,6 +24,16 @@
 @stop
 
 @section ('content')
-
+<div class="row content">
+	<div class="col-md-2 col-md-offset-1">
+		<img src="{{ asset('images/headshot.png') }}" alt="Abhijit Pradhan" width="100">
+	</div>
+	<div class="col-md-5 profile">
+		<p>
+		Productive and resourceful web professional seeking front-end web development opportunities in Chicago, with experience in Web Design and Customer Service. Quick to learn new 
+programming languages and technologies. Enjoys creative problem solving and skilled at clearly communicating technical issues with team members.
+		</p>
+	</div>
+</div>
 @stop
 	
